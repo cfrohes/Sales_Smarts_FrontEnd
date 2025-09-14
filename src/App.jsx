ec1100e1deee0e3439ce64c6a28e447c9cc92721
cfrohes <chandanshangami@gmail.com>
2025-09-14 17:18:56 +0530

    Frontend push to new GitHub repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { BrowserRouter as Router } from 'react-router-dom'; 
-import AppRoutes from './AppRoutes'; 
+import AppRoutes from './Routes'; 
 import './assets/styles.css'; 
 function App() 
 { 
